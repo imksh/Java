fe4a6d335abd87cdb6c39f020ff4c1597150f9ac
Karan Sharma <karan03945@gmail.com>
2025-08-02 02:42:55 +0530

    initial add

---
+++ practice/src/Main.java
@@ -0,0 +1,19 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("helllo java");
+        int sum = add(5, 7);
+        System.out.println("Sum: " + sum);
+    }
+
+    public static int add(int a, int b) 
+    {
+        return a + b;
+    }
+
+    public static int add(int a, int b,int c, int d) 
+    {
+        return a + b + c + d;
+    }
+}
